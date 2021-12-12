33166a3f9093617448c19d5f9905e3cbe692b84e
Lazytangent <pm123633@gmail.com>
2021-12-11 18:38:13 -0600

    Tree sitter config for x86

--- packages.el
+++ packages.el
@@ -50,15 +50,8 @@
 ;(unpin! t)
 (add-load-path! "~/Documents/Emacs/elisp-tree-sitter")
 (package! pipenv)
-(package! tsc
-  :recipe (:local-repo "~/Documents/Emacs/elisp-tree-sitter/core"
-           :files ("DYN-VERSION" "tsc-dyn.*" "*.el")))
+(package! tsc)
 (package! tree-sitter)
-;; (package! tree-sitter-hl)
 (package! tree-sitter-langs
-  :recipe (:local-repo "~/Documents/Emacs/elisp-tree-sitter"
-           :files ("langs/*.el" ("bin" "langs/bin/*.dylib") ("queries" "langs/queries/*"))))
 (package! org-super-agenda)
 (package! emmet-mode)
-;; (package! tree-sitter-debug)
-;; (package! tree-sitter-query)
