e22cb8b3fb0b9e05004349a4c08aa73e6448da2d
Peter Mai <peter.tn.mai@gmail.com>
2022-07-10 12:29:11 -0500

    Tree-sitter for arch based system

--- packages.el
+++ packages.el
@@ -1,6 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 
 (package! pipenv)
+(package! tsc)
+(package! tree-sitter)
+(package! tree-sitter-langs)
 (package! org-super-agenda)
 (package! emmet-mode)
 (package! graphql-mode)
